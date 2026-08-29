75b92b15cfeeb889ac1cd5125e69bffd39627eea
juan josee <juanjose.sepu50@gmail.com>
2026-08-29 12:59:45 -0500

    Agregar variable de entorno para el nombre de la base de datos en la configuración

--- CONFIG_DATABASE/config.php
+++ CONFIG_DATABASE/config.php
@@ -3,6 +3,7 @@
 $host = getenv('DB_HOST') ?: 'localhost';
 $user = getenv('DB_USER') ?: 'root';
 $pass = getenv('DB_PASS') ?: '';
+$db   = getenv('DB_NAME') ?: 'biblioteca';
 
 $conexion = new mysqli($host, $user, $pass, $db);
 
